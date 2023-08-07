ddb69f20359a1c01a7c15acbdcefde0016c64a0d
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-08-07 15:57:01 +0900

    :wrench: 컨트롤러 추가

---
+++ src/main/java/com/example/todo/todo/controller/TodoController.java
@@ -0,0 +1,15 @@
+package com.example.todo.todo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TodoController {
+    @GetMapping
+    public String getTodo(){
+        return "To-do Application!";
+    }
+}
